bbc8f1465b992f1213f8ccef14b7b86ee1c2159b
PHoulding <phouldin@uoguelph.ca>
2017-11-06 11:43:16 -0500

    Models finished

--- CLC_FASD/models.py
+++ CLC_FASD/models.py
@@ -4,3 +4,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+	uType = (('Participant','Participant'),('Staff','Staff'),('Admin','Admin'),('Family/Friends','Family/Friends'))
+	username = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+	sessionKey = models.CharField(max_length=50)
+	userType = models.CharField(max_length=10,choices=uType,default='Participant')
+	medication = models.CharField(max_length=50)
+	connection = models.CharField(max_length=50)
+
+class Medication(models.Model):
+	daysOfWeek=(('Monday','Monday'),('Tuesday','Tuesday'), \
+		('Wednesday','Wednesday'),('Thursday','Thursday'), \
+		('Friday','Friday'),('Saturday','Saturday'),('Sunday','Sunday'))
+	name = models.CharField(max_length=50)
+	time = models.TimeField(blank=True,auto_now_add=False)
+	day = models.CharField(max_length=10,choices=daysOfWeek)
+	user = models.ForeignKey(Users,related_name='medication')
+
+class Connections(models.Model):
+	username = models.CharField(max_length=50)
+	connection = models.ForeignKey(Users,related_name='connection')
