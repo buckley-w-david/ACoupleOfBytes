9e566c18a842e67861af8426c50a50d4a3ffbb9f
David Buckley <dbuckl02@mail.uoguelph.ca>
2017-11-18 18:24:47 -0500

    Some changes to login to make my life easier

---
+++ CLC_FASD/APICalls/account.py
@@ -0,0 +1,37 @@
+from CLC_FASD import models
+import uuid
+
+#username, password
+def login(un, pw):
+    if Users.objects.filter(username=un).exists():
+        if Users.objects.filter(username=un, password=pw).exists():
+            sk = uuid.uuid1()
+            user = Users.objects.get(username=un, password=pw)
+            user.sessionKey = sk
+            user.save()
+            return str(sk)
+        else:
+            return False
+    else:
+        return False
+
+#session key
+def logout(sk):
+    if sk == "":
+        return False
+    elif Users.objects.filter(sessionKey=sk).exists():
+        user = Users.objects.get(sessionKey=sk)
+        user.sessionKey = ""
+        user.save()
+        return True
+    else:
+        return False
+
+#session key
+def getUsernameBySessionKey(sk):
+    if Users.objects.filter(sessionKey=sk).exists():
+        user = Users.objects.get(sessionKey=sk)
+        un = user.username
+        return str(un)
+    else:
+        return False
