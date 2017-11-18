f594b92a93a9b4bdd0232889483cb82933189680
David Buckley <dbuckl02@mail.uoguelph.ca>
2017-11-17 22:56:30 -0500

    fix PYTHONPATH issue

--- export_pypath.sh
+++ export_pypath.sh
@@ -1,6 +1,9 @@
 script="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 package="${script}/../.."
 abs_package=`readlink -f $package`
-export _OLD_PYTHONPATH=$PYTHONPATH
+
+_OLD_PYTHONPATH="$PYTHONPATH"
+export _OLD_PYTHONPATH="$_OLD_PYTHONPATH"
+
 PYTHONPATH="$abs_package"
-export PYTHONPATH
+export PYTHONPATH="$PYTHONPATH"
